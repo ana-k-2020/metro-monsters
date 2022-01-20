ec61a8c049d3f659d607c528483d5bdf171dece5
ana-k-2020 <69907376+ana-k-2020@users.noreply.github.com>
2022-01-20 17:56:10 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,77 @@
+import file_operations
+from faker import Faker
+import random
+
+
+def npc():
+    name = fake.name().split()
+    town = fake.city()
+    job = fake.job()
+
+    char_skills = random.sample(skills, 3)
+    runic_skills = []
+    for skill in char_skills:
+        for letter in skill:
+            for normal, runic in letters_mapping.items():
+                if letter == normal:
+                    skill = skill.replace(letter, runic)
+        runic_skills.append(skill)
+
+    global context
+    context = {
+        'first_name': name[0],
+        'last_name': name[2],
+        'town': town,
+        'job': job,
+        'strength': random.randrange(3, 19),
+        'agility': random.randrange(3, 19),
+        'endurance': random.randrange(3, 19),
+        'intelligence': random.randrange(3, 19),
+        'luck': random.randrange(3, 19),
+        'skill_1': runic_skills[0],
+        'skill_2': runic_skills[1],
+        'skill_3': runic_skills[2]
+    }
+
+
+if __name__ == '__main__':
+    fake = Faker('ru_RU')
+    skills = [
+        'Стремительный прыжок',
+        'Электрический выстрел',
+        'Ледяной удар',
+        'Стремительный удар',
+        'Кислотный взгляд',
+        'Тайный побег',
+        'Ледяной выстрел',
+        'Огненный заряд'
+    ]
+    letters_mapping = {
+        'а': 'а͠', 'б': 'б̋', 'в': 'в͒͠',
+        'г': 'г͒͠', 'д': 'д̋', 'е': 'е͠',
+        'ё': 'ё͒͠', 'ж': 'ж͒', 'з': 'з̋̋͠',
+        'и': 'и', 'й': 'й͒͠', 'к': 'к̋̋',
+        'л': 'л̋͠', 'м': 'м͒͠', 'н': 'н͒',
+        'о': 'о̋', 'п': 'п̋͠', 'р': 'р̋͠',
+        'с': 'с͒', 'т': 'т͒', 'у': 'у͒͠',
+        'ф': 'ф̋̋͠', 'х': 'х͒͠', 'ц': 'ц̋',
+        'ч': 'ч̋͠', 'ш': 'ш͒͠', 'щ': 'щ̋',
+        'ъ': 'ъ̋͠', 'ы': 'ы̋͠', 'ь': 'ь̋',
+        'э': 'э͒͠͠', 'ю': 'ю̋͠', 'я': 'я̋',
+        'А': 'А͠', 'Б': 'Б̋', 'В': 'В͒͠',
+        'Г': 'Г͒͠', 'Д': 'Д̋', 'Е': 'Е',
+        'Ё': 'Ё͒͠', 'Ж': 'Ж͒', 'З': 'З̋̋͠',
+        'И': 'И', 'Й': 'Й͒͠', 'К': 'К̋̋',
+        'Л': 'Л̋͠', 'М': 'М͒͠', 'Н': 'Н͒',
+        'О': 'О̋', 'П': 'П̋͠', 'Р': 'Р̋͠',
+        'С': 'С͒', 'Т': 'Т͒', 'У': 'У͒͠',
+        'Ф': 'Ф̋̋͠', 'Х': 'Х͒͠', 'Ц': 'Ц̋',
+        'Ч': 'Ч̋͠', 'Ш': 'Ш͒͠', 'Щ': 'Щ̋',
+        'Ъ': 'Ъ̋͠', 'Ы': 'Ы̋͠', 'Ь': 'Ь̋',
+        'Э': 'Э͒͠͠', 'Ю': 'Ю̋͠', 'Я': 'Я̋',
+        ' ': ' '
+    }
+    for i in range(10):
+        npc()
+        card_name = 'characters/char_card{}.svg'.format(i+1)
+        file_operations.render_template('charsheet.svg', card_name, context)
